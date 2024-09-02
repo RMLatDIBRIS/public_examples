19967efeaa6158fd9b0a82671b8dedb334d07767
davideancona <davide.ancona@unige.it>
2024-09-02 23:39:08 +0200

    site renamed:    site/data-types/lists/insert-remove-size-get-single-old.rml -> site/data-types/lists/insert-remove-size-get-single.rml.old new file:   site/resources/exclusive2-trace.txt new file:   site/resources/exclusive2.pl

---
+++ site/resources/exclusive2.pl
@@ -0,0 +1,17 @@
+:- module(spec, [(trace_expression/2), (match/2)]).
+:- use_module(monitor(deep_subdict)).
+:- use_module(library(clpr)).
+match(_event, acquire_et(Eid, Rid)) :- deep_subdict(_event, _{event:"func_pre", name:"acquire", args:[Eid, Rid|_]}).
+match(_event, release_et(Eid, Rid)) :- deep_subdict(_event, _{event:"func_pre", name:"release", args:[Eid, Rid|_]}).
+match(_event, use_et(Eid, Rid)) :- deep_subdict(_event, _{event:"func_pre", name:"use", args:[Eid, Rid|_]}).
+match(_event, notAcqRel_et(Eid, Rid)) :- not(match(_event, acquire_et(_, Rid))),
+	not(match(_event, release_et(Eid, Rid))).
+match(_event, notAcq_et) :- not(match(_event, acquire_et(_, _))).
+match(_event, relevant_et) :- match(_event, acquire_et(_, _)).
+match(_event, relevant_et) :- match(_event, release_et(_, _)).
+match(_event, relevant_et) :- match(_event, use_et(_, _)).
+match(_event, any_et) :- deep_subdict(_event, _{}).
+match(_event, none_et) :- not(match(_event, any_et)).
+trace_expression('Main', Main) :- (Main=((relevant_et>>(UseAfterAcquire/\NoMultipleAcquire));1)),
+	(UseAfterAcquire=optional(var(eid, var(rid, (acquire_et(var(eid), var(rid))*((star(use_et(var(eid), var(rid)))*release_et(var(eid), var(rid)))|UseAfterAcquire)))))),
+	(NoMultipleAcquire=(star(notAcq_et)*optional(var(eid, var(rid, (acquire_et(var(eid), var(rid))*((star(notAcqRel_et(var(eid), var(rid)))*(release_et(var(eid), var(rid))*1))/\NoMultipleAcquire))))))).
